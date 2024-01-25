444caac22a2b3417d5975c114afdc06cf51aa2a3
EmirB08 <142892269+EmirB08@users.noreply.github.com>
2024-01-25 11:30:36 +0100

    KommunerDropdown.jsx + JoyUI

---
+++ src/components/KommunerDropdown.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import Select from "@mui/joy/Select";
+import Option from "@mui/joy/Option";
+
+const KommunerDropdown = () => {
+    const [kommuner, setKommuner] = useState([]);
+
+    useEffect(() => {
+        fetch("https://data.ssb.no/api/klass/v1/versions/1847.json")
+            .then(response => response.json())
+            .then(data => {
+                setKommuner(data.classificationItems);
+            });
+    }, []);
+
+    return (
+        <Select>
+            {kommuner.map(({ code, name }) => (
+                <Option key={code} value={code}>{name}</Option>
+            ))}
+        </Select>
+    );
+};
+
+export default KommunerDropdown;
