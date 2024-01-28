733f8884e9b33983c3a75839b49feac2e1b65d05
EmirB08 <142892269+EmirB08@users.noreply.github.com>
2024-01-29 00:30:17 +0100

    css

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -6,7 +6,7 @@ const MainContainer = ({ children }) => {
     return (
         <React.Fragment>
             <Container maxWidth="md">
-                <Box sx={{ bgcolor: "#f8f8f8d8;" }}>
+                <Box sx={{ bgcolor: "#f8f8f8d8;", height:"100vh" }}>
                     {children}
                 </Box>
             </Container>
