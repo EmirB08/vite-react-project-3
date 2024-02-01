f805da86d38fd7a0e8807c4e30363b0ca4df9532
EmirB08 <142892269+EmirB08@users.noreply.github.com>
2024-02-01 21:29:03 +0100

    Update UserContext.jsx

--- src/components/UserContext.jsx
+++ src/components/UserContext.jsx
@@ -4,19 +4,20 @@ import PropTypes from "prop-types";
 export const UserContext = createContext();
 
 /**
- * UserInput komponent gir context  som holder delte values for applikasjonen
- * verdier kan bli oppdaterert fra UserContext for alle komponenter under UserInput fra app.jsx
+ * React context for å dele state mellom komponenter.
+ * Verdier kan bli oppdaterert fra UserContext for alle komponenter under UserInput fra app.jsx
  * inneholder foreløpig kommuneCode og year.
+ * Alle oppdateringer til values via setKommuneCode og setYear blir delt med alle komponenter som har tilgang til UserContext.
  * 
  * @param {Object} props - props for komponenten
- * @param {ReactNode} props.children - komponenter under UserInput i komponenttreet deler values fra UserContext
+ * @param {ReactNode} props.children - komponenter som skal ha tilgang til context values
  */
 
 export const UserInput = ({ children }) => {
     const [kommuneCode, setKommuneCode] = useState(null);
     const [year, setYear] = useState(null);
 
-    const value = { // lager et value object med props som skal bli delt med alle komponenter
+    const value = { // konstruer et objekt med alle values og funksjoner som skal deles
         kommuneCode,
         setKommuneCode,
         year,
