257b1bea5a1ff189b1a813d7fba271f36db1119a
EmirB08 <142892269+EmirB08@users.noreply.github.com>
2024-02-01 21:30:13 +0100

    Update UserContext.jsx

--- src/components/UserContext.jsx
+++ src/components/UserContext.jsx
@@ -5,7 +5,6 @@ export const UserContext = createContext();
 
 /**
  * React context for å dele state mellom komponenter.
- * Verdier kan bli oppdaterert fra UserContext for alle komponenter under UserInput fra app.jsx
  * inneholder foreløpig kommuneCode og year.
  * Alle oppdateringer til values via setKommuneCode og setYear blir delt med alle komponenter som har tilgang til UserContext.
  * 
